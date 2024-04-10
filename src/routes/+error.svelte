69b909151536443e2e7a4fe2f05f2e0e71c84951
levtus <i@alexander.systems>
2024-04-10 10:34:54 -0500

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    let hostname = './';
+    onMount(() => {
+        hostname = window.location.origin;
+    });
+</script>
+<div class="flex flex-col items-center justify-center font-bold pt-[30vh] px-4">
+    <p class="text-2xl md:text-4xl font-bold pb-2">404</p>
+    <h1 class="text-4xl md:text-6xl font-bold">Page not Found</h1>
+    <a href={hostname} class="text-3xl font-bold pt-5">Go Home</a>
+</div>
