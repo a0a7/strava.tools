ad488d4518595ee3d679509e72d764a4d6661074
levtus <i@alexander.systems>
2024-04-11 17:58:35 -0500

    Fix dark mode light mode button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -44,9 +44,9 @@
             </a>
             <button on:click={toggleMode} class="pl-2">
                 {#if $mode === 'dark'}
-                    <img src="img/icon/switch_from_dark.svg" alt="Switch Theme" class="w-5 h-5 dark:invert" title="Switch theme"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-white" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
                 {:else}
-                    <img src="img/icon/switch_from_light.svg" alt="Switch Theme" class="w-5 h-5 dark:invert" title="Switch theme"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                 {/if}
             </button>
         </div>
