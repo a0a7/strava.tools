128051266bffb0cbb2af4f991e20e7d0ba029d0c
levtus <i@alexander.systems>
2024-04-11 21:38:03 -0500

    Strip deprecated svelte

--- src/lib/components/sidebar/MapConfigPanel.svelte
+++ src/lib/components/sidebar/MapConfigPanel.svelte
@@ -7,27 +7,15 @@
 	import { Label } from '$lib/components/ui/label';
 	import { Switch } from '$lib/components/ui/switch';
 	import BasemapSelect from '$lib/components/sidebar/map-config-components/BasemapSelect.svelte';
-	import CommuteSelect from '$lib/components/sidebar/map-config-components/CommuteSelect.svelte';
-	import ActivityTypeSelect from '$lib/components/sidebar/map-config-components/ActivityTypeSelect.svelte';
-	import DateRangePicker from '$lib/components/sidebar/map-config-components/DateRangePicker.svelte';
+	import ActivityTypeSelect from '$lib/components/settings/ActivityTypeSelect.svelte';
 	import type { StravaActivity } from '$lib/activities';
 
-	export let activities: StravaActivity[];
 	export let activityTypeFilter: string;
-	export let commuteFilter: string;
-	export let dateRangeMinFilter: string;
-	export let dateRangeMaxFilter: string;
 	export let showPrivate: boolean = true;
 </script>
 
 <div class="w-full px-6 flex flex-col gap-3">
 	<ActivityTypeSelect bind:value={activityTypeFilter} />
-	<CommuteSelect bind:value={commuteFilter} />
-	<DateRangePicker
-		{activities}
-		bind:startDate={dateRangeMinFilter}
-		bind:endDate={dateRangeMaxFilter}
-	/>
 	<div class="pl-2 pt-s1">
 		<Label for="private-activities" class="bottom-1 pr-1 relative select-none text-sm font-medium"
 			>Show Private Activities</Label
